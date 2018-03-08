ae20fe0f7bae6529f84041b90d24bc5ce32beffc
Imron Khatib <imronskhatib@gmail.com>
2018-03-07 20:29:10 -0600

    create if statement for users with name starting with the letter a

--- app/controllers/v1/names_controller.rb
+++ app/controllers/v1/names_controller.rb
@@ -1,9 +1,13 @@
 class V1::NamesController < ApplicationController
 
   def your_name
+    
+    the_name = params["name"]
 
-    the_name = params["name"].upcase
-
-    render json: {"Your name is": the_name}
+      if the_name[0] == "a"
+        render json:{"Your name is": the_name, message: "Hey your name begins with the first letter of the alphabet!"}
+      else    
+        render json: {"Your name is": the_name}
+    end
   end
 end
