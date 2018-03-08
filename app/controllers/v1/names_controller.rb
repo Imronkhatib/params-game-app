f8097642d7e600ace48668edf2d38005b319100a
Imron Khatib <imronskhatib@gmail.com>
2018-03-07 20:31:44 -0600

    Modify param to accept upcased A

--- app/controllers/v1/names_controller.rb
+++ app/controllers/v1/names_controller.rb
@@ -2,9 +2,9 @@ class V1::NamesController < ApplicationController
 
   def your_name
     
-    the_name = params["name"]
+    the_name = params["name"].upcase
 
-      if the_name[0] == "a"
+      if the_name[0] == "A"
         render json:{"Your name is": the_name, message: "Hey your name begins with the first letter of the alphabet!"}
       else    
         render json: {"Your name is": the_name}
