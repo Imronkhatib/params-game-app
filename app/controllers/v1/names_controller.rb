310acec7a66d411d13c874974f2c1615beb298c6
Imron Khatib <imronskhatib@gmail.com>
2018-03-07 20:12:13 -0600

    Create initial Params/ test Params

--- app/controllers/v1/names_controller.rb
+++ app/controllers/v1/names_controller.rb
@@ -2,8 +2,8 @@ class V1::NamesController < ApplicationController
 
   def your_name
 
+    the_name = params["name"].upcase
 
-
-    render json: {message: "hello"}
+    render json: {"Your name is": the_name}
   end
 end
