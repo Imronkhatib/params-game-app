8541189339d2b4a3420b6741f1742ad20960d6d7
Imron Khatib <imronskhatib@gmail.com>
2018-03-07 20:41:01 -0600

    Improve method name from the_name to message

--- app/controllers/v1/names_controller.rb
+++ app/controllers/v1/names_controller.rb
@@ -2,12 +2,12 @@ class V1::NamesController < ApplicationController
 
   def your_name
     
-    the_name = params["name"].upcase
+    message = params["name"].upcase
 
-      if the_name[0] == "A"
-        render json:{"Your name is": the_name, message: "Hey your name begins with the first letter of the alphabet!"}
+      if message[0] == "A" 
+        render json:{"Your name is": "Hey your name begins with the first letter of the alphabet!"}
       else    
-        render json: {"Your name is": the_name}
+        render json: {"Your name is": message}
     end
   end
 end
